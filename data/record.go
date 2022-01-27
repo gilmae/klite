05031608c27922843c78b32668e4995af1b95bee
gilmae <avocadia@fastmail.fm>
2022-01-27 12:49:14 +0000

    Adds methods for serialising and deserialising the Record sruct

--- data/record.go
+++ data/record.go
@@ -1,6 +1,23 @@
 package data
 
+import "encoding/binary"
+
 type Record struct {
 	pageNum uint32
 	length  uint32
+}
+
+func Deserialise(enc []byte) Record {
+	r := Record{}
+	r.pageNum = binary.LittleEndian.Uint32(enc[0:4])
+	r.length = binary.LittleEndian.Uint32(enc[4:8])
+
+	return r
+}
+
+func Serialise(r Record) []byte {
+	enc := make([]byte, 8)
+	binary.LittleEndian.PutUint32(enc[0:4], r.pageNum)
+	binary.LittleEndian.PutUint32(enc[4:8], r.length)
+	return enc
 }
---
+++ data/record_test.go
@@ -0,0 +1,28 @@
+package data
+
+import "testing"
+
+func TestSerialise(t *testing.T) {
+	r := Record{pageNum: 3, length: 513}
+	enc := Serialise(r)
+	expectedValue := []byte{3, 0, 0, 0, 1, 2, 0, 0}
+	if !bytesMatch(enc, expectedValue) {
+		t.Errorf("incorrect serialised value, expected %+v, got %+v", expectedValue, enc)
+	}
+}
+
+func TestDeserialise(t *testing.T) {
+	bytes := []byte{7, 0, 0, 0, 3, 1, 0, 0}
+	r := Deserialise(bytes)
+
+	expectedPageNum := uint32(7)
+	expectedLength := uint32(259)
+
+	if r.pageNum != expectedPageNum {
+		t.Errorf("incorrect r.pageNum, expected %d, got %d", expectedPageNum, r.pageNum)
+	}
+
+	if r.length != expectedLength {
+		t.Errorf("incorrect r.length, expected %d, got %d", expectedLength, r.length)
+	}
+}
