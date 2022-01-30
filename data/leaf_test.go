e55d867e35ada9a8e5198a6bfe23f35265eb1409
gilmae <avocadia@fastmail.fm>
2022-01-30 15:09:01 +1100

    Move leaf finding, inserting, and splitting methods to tree

--- data/leaf_test.go
+++ data/leaf_test.go
@@ -98,66 +98,6 @@ func TestSetLeafValue(t *testing.T) {
 	}
 }
 
-func TestLeafFind(t *testing.T) {
-	tests := []struct {
-		key              uint32
-		expectedPosition uint16
-		expectedFound    bool
-	}{
-		{1, 0, true},
-		{2, 1, true},
-		{5, 3, true},
-		{3, 2, true},
-		{6, 4, false}, // key is not present but would be in index 4 if it were inserted
-		{4, 3, false}, // key is not present but would be in index 3 if it were inserted
-	}
-
-	page := Page(make([]byte, PageSize))
-	copy(page[0:], []byte{0, 0, 0, 0, 0, 0, 4, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0})
-	leaf := NewNode(&page)
-	for _, test := range tests {
-		c, found := leaf.leafNodeFind(test.key)
-		if c.Index != test.expectedPosition {
-			t.Errorf("unexpected position for key %d, expected %d, got %d", test.key, test.expectedPosition, c.Index)
-		}
-
-		if found != test.expectedFound {
-			t.Errorf("unexpected found flag for key %d, expected %t, got %t", test.key, test.expectedFound, found)
-		}
-	}
-}
-
-func TestLeafInsert(t *testing.T) {
-	page := Page(make([]byte, PageSize))
-	copy(page[0:], []byte{0, 0, 0, 0, 0, 0, 4, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0})
-	leaf := NewNode(&page)
-
-	tests := []struct {
-		cell          uint16
-		key           uint32
-		value         Record
-		expectedBytes []byte
-	}{
-		{0, 1, Record{1, 2}, []byte{1, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0}},
-		{1, 2, Record{3, 4}, []byte{2, 0, 0, 0, 3, 0, 0, 0, 4, 0, 0, 0}},
-		{2, 3, Record{5, 6}, []byte{3, 0, 0, 0, 5, 0, 0, 0, 6, 0, 0, 0}},
-		{3, 4, Record{7, 8}, []byte{4, 0, 0, 0, 7, 0, 0, 0, 8, 0, 0, 0}},
-		{4, 5, Record{9, 10}, []byte{5, 0, 0, 0, 9, 0, 0, 0, 10, 0, 0, 0}},
-	}
-
-	leaf.leafInsert(2, 3, Record{5, 6})
-	leaf.leafInsert(4, 5, Record{9, 20})
-
-	for _, test := range tests {
-		cellOffset := LeafNodeHeaderSize + test.cell*LeafNodeCellSize
-		leaf.leafInsert(test.cell, test.key, test.value)
-		bytes := (*leaf.page)[cellOffset : cellOffset+LeafNodeCellSize]
-		if !bytesMatch(bytes, test.expectedBytes) {
-			t.Errorf("incorrect bytes found at cell %d, expected %+v, got %+v", test.key, test.expectedBytes, bytes)
-		}
-	}
-}
-
 func bytesMatch(x, y []byte) bool {
 	if len(x) != len(y) {
 		return false
--- data/tree.go
+++ data/tree.go
@@ -2,8 +2,71 @@ package data
 
 type Tree struct {
 	pager Pager
+	root  *Node
 }
 
-func NewTree(pager Pager) *Tree {
-	return &Tree{pager: pager}
+func (t Tree) Get(key uint32) Record {
+	c, found := leafNodeFind(t.root, key)
+	if found {
+		return t.root.GetNodeValue(c.Index)
+	}
+	return Record{}
 }
+
+// leafNodeFind returns the position in the node the key should be in. The key may not actually be present
+func leafNodeFind(n *Node, key uint32) (Cursor, bool) {
+	numCells := n.NumCells()
+	minIndex := uint16(0)
+	onePastMaxIndex := numCells
+
+	c := Cursor{n, 0}
+	for onePastMaxIndex != minIndex {
+
+		idx := (minIndex + onePastMaxIndex) / 2
+		keyAtIndex := n.GetNodeKey(idx)
+
+		if key == keyAtIndex {
+			c.Index = idx
+			return c, true
+		}
+		if key < keyAtIndex {
+			onePastMaxIndex = idx
+		} else {
+			minIndex = idx + 1
+		}
+	}
+	c.Index = minIndex
+	return c, false
+}
+
+func (t *Tree) leafInsert(n *Node, cell uint16, key uint32, data Record) {
+	// If leaf is already full, need to call leafSplitAndInsert
+	numCells := n.NumCells()
+	if numCells >= LeafNodeMaxCells {
+		return
+	}
+
+	// Make space for new cell if it is not at the right hand end
+	if cell < numCells {
+		for i := numCells; i > cell; i-- {
+			n.setNodeCell(i, n.getNodeCell(i-1))
+		}
+	}
+
+	// Insert at the requested cell
+	n.SetNumCells(numCells + 1)
+	n.SetNodeKey(cell, key)
+	n.SetNodeValue(cell, data)
+}
+
+func (l *Node) leafSplitAndInsert(key uint32, value Record) {
+
+}
+
+// func (t *Tree) splitLeafAndInsert(cursor *Cursor, key uint32, value Record) {
+// 	nextPageNum := t.pager.GetNextUnusedPageNum()
+// 	newPage, _ := t.pager.Page(nextPageNum) // TODO handle error
+// 	newLeaf := Node{newPage}
+
+// 	newLeaf
+// }
