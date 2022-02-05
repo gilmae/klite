4a8872db982cfb2bf4a7dd4655ddd8e826878832
gilmae <avocadia@fastmail.fm>
2022-02-05 15:11:28 +1100

    Adds internal nodes

---
+++ data/internal.go
@@ -0,0 +1,65 @@
+package data
+
+import (
+	"encoding/binary"
+	"unsafe"
+)
+
+const (
+	NumKeysSize            = uint16(unsafe.Sizeof(uint16(0)))
+	NumKeysOffset          = GenericHeaderSize
+	RightChildSize         = uint16(unsafe.Sizeof(uint32(0)))
+	RightChildOffset       = NumKeysOffset + NumKeysSize
+	InternalNodeHeaderSize = GenericHeaderSize + NumKeysSize + RightChildSize
+
+	InternalNodeKeySize   = uint16(unsafe.Sizeof(uint32(0)))
+	InternalNodeChildSize = uint16(unsafe.Sizeof(uint32(0)))
+	InternalNodeCellSize  = InternalNodeKeySize + InternalNodeChildSize
+)
+
+func NewInternal(data *Page) *Node {
+	n := Node{data}
+	n.SetType(InternalNode)
+	n.SetIsRoot(false)
+	n.SetNumKeys(0)
+	return &n
+}
+
+func (n *Node) NumKeys() uint16 {
+	return binary.LittleEndian.Uint16((*n.page)[NumKeysOffset : NumKeysOffset+NumKeysSize])
+}
+
+func (n *Node) SetNumKeys(c uint16) {
+	binary.LittleEndian.PutUint16((*n.page)[NumKeysOffset:NumKeysOffset+NumKeysSize], c)
+}
+
+func (n *Node) RightChild() uint32 {
+	return binary.LittleEndian.Uint32((*n.page)[RightChildOffset : RightChildOffset+RightChildSize])
+}
+
+func (n *Node) SetRightChild(c uint32) {
+	binary.LittleEndian.PutUint32((*n.page)[RightChildOffset:RightChildOffset+RightChildSize], c)
+}
+
+func (n *Node) internalCell(cell uint16) []byte {
+	cellOffset := InternalNodeHeaderSize + InternalNodeCellSize*cell
+	return (*n.page)[cellOffset : cellOffset+InternalNodeCellSize]
+}
+
+func (n *Node) ChildPointer(cell uint16) uint32 {
+	return binary.LittleEndian.Uint32(n.internalCell(cell)[0:InternalNodeChildSize])
+}
+
+func (n *Node) SetChildPointer(cell uint16, ptr uint32) {
+	c := n.internalCell(cell)
+	binary.LittleEndian.PutUint32(c[0:InternalNodeChildSize], ptr)
+}
+
+func (n *Node) InternalKey(cell uint16) uint32 {
+	return binary.LittleEndian.Uint32(n.internalCell(cell)[InternalNodeChildSize : InternalNodeChildSize+InternalNodeKeySize])
+}
+
+func (n *Node) SetInternalKey(cell uint16, key uint32) {
+	c := n.internalCell(cell)
+	binary.LittleEndian.PutUint32(c[InternalNodeChildSize:InternalNodeChildSize+InternalNodeKeySize], key)
+}
---
+++ data/internal_test.go
@@ -0,0 +1,154 @@
+package data
+
+import "testing"
+
+func TestGetNumKeys(t *testing.T) {
+
+	p := Page{0x1, 0x0, 0x0, 0x0, 0x0, 0x0, 0x2, 0x0}
+	expectedNumKeys := 2
+	node := Node{page: &p}
+
+	actualNumKeys := node.NumKeys()
+
+	if expectedNumKeys != int(actualNumKeys) {
+		t.Errorf("incorrect number of keys, expected %d, got %d", expectedNumKeys, actualNumKeys)
+	}
+
+}
+
+func TestSetNumKeys(t *testing.T) {
+	p := Page(make([]byte, PageSize))
+	node := NewInternal(&p)
+
+	node.SetNumKeys(6)
+
+	expectedData := []byte{0x6, 0x0}
+	actualData := p[6:8]
+
+	if !bytesMatch(expectedData, actualData) {
+		t.Errorf("incorrect bytes for NumKeys set, expected %+v, got %+v", expectedData, actualData)
+	}
+}
+
+func TestRightChild(t *testing.T) {
+	p := Page{0x1, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x0, 0x1, 0x1, 0x0, 0x0}
+	node := Node{page: &p}
+
+	expectedResult := uint32(257)
+	actualResult := node.RightChild()
+
+	if expectedResult != actualResult {
+		t.Errorf("incorrect value for node.RightChild, expected %d, got %d", expectedResult, actualResult)
+	}
+
+}
+
+func TestSetRightChild(t *testing.T) {
+	p := Page(make([]byte, PageSize))
+	node := NewInternal(&p)
+
+	node.SetRightChild(264)
+
+	expectedData := []byte{0x8, 0x1, 0x0, 0x0}
+	actualData := p[8:12]
+
+	if !bytesMatch(expectedData, actualData) {
+		t.Errorf("incorrect bytes for NumKeys set, expected %+v, got %+v", expectedData, actualData)
+	}
+}
+
+func TestChildPointer(t *testing.T) {
+	tests := []struct {
+		cell           uint16
+		data           []byte
+		expectedResult uint32
+	}{
+		{0, []byte{0x0, 0x1, 0x0, 0x0}, 256},
+		{10, []byte{0x1, 0x1, 0x1, 0x1}, 16843009},
+	}
+
+	for _, test := range tests {
+		p := Page(make([]byte, PageSize))
+		node := NewInternal(&p)
+		offset := 12 + 8*int(test.cell)
+		copy(p[offset:offset+4], test.data)
+		actualResult := node.ChildPointer(test.cell)
+
+		if test.expectedResult != actualResult {
+			t.Errorf("Incorrect value for node.ChildPointer, exected %d, got %d", test.expectedResult, actualResult)
+		}
+	}
+
+}
+
+func TestSetChildPointer(t *testing.T) {
+	tests := []struct {
+		cell           uint16
+		data           uint32
+		expectedResult []byte
+	}{
+		{0, 256, []byte{0x0, 0x1, 0x0, 0x0}},
+		{10, 16843008, []byte{0x0, 0x1, 0x1, 0x1}},
+	}
+
+	for _, test := range tests {
+		p := Page(make([]byte, PageSize))
+		node := NewInternal(&p)
+		node.SetChildPointer(test.cell, test.data)
+		offset := 12 + 8*int(test.cell)
+		actualResult := p[offset : offset+4]
+
+		if !bytesMatch(test.expectedResult, actualResult) {
+			t.Errorf("Incorrect data set by node.SetChildPointer, exected %+v, got %+v", test.expectedResult, actualResult)
+		}
+	}
+
+}
+
+func TestInternalKey(t *testing.T) {
+	tests := []struct {
+		cell           uint16
+		data           []byte
+		expectedResult uint32
+	}{
+		{0, []byte{0x0, 0x1, 0x0, 0x0}, 256},
+		{10, []byte{0x1, 0x1, 0x1, 0x1}, 16843009},
+	}
+
+	for _, test := range tests {
+		p := Page(make([]byte, PageSize))
+		node := NewInternal(&p)
+		offset := 16 + 8*int(test.cell)
+		copy(p[offset:offset+4], test.data)
+		actualResult := node.InternalKey(test.cell)
+
+		if test.expectedResult != actualResult {
+			t.Errorf("Incorrect value for node.ChildPointer, exected %d, got %d", test.expectedResult, actualResult)
+		}
+	}
+
+}
+
+func TestSetTestInternalKey(t *testing.T) {
+	tests := []struct {
+		cell           uint16
+		data           uint32
+		expectedResult []byte
+	}{
+		{0, 257, []byte{0x1, 0x1, 0x0, 0x0}},
+		{10, 16843010, []byte{0x2, 0x1, 0x1, 0x1}},
+	}
+
+	for _, test := range tests {
+		p := Page(make([]byte, PageSize))
+		node := NewInternal(&p)
+		node.SetInternalKey(test.cell, test.data)
+		offset := 16 + 8*int(test.cell)
+		actualResult := p[offset : offset+4]
+
+		if !bytesMatch(test.expectedResult, actualResult) {
+			t.Errorf("Incorrect data set by node.SetChildPointer, exected %+v, got %+v", test.expectedResult, actualResult)
+		}
+	}
+
+}
