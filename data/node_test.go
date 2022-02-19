8cafe6aa8a05a44712eb77aca383b32a5e4cdb41
gilmae <avocadia@fastmail.fm>
2022-02-19 17:22:54 +1100

    Adds tests for IsRoot

--- data/node_test.go
+++ data/node_test.go
@@ -39,3 +39,41 @@ func TestSetNodeType(t *testing.T) {
 	}
 
 }
+
+func TestIsRoot(t *testing.T) {
+	tests := []struct {
+		page          Page
+		expectedValue bool
+	}{
+		{Page{0, 1}, true},
+		{Page{1, 0}, false},
+	}
+
+	for _, test := range tests {
+		n := NewNode(&test.page)
+		if n.IsRoot() != test.expectedValue {
+			t.Errorf("node is wrong type ,expected %+v, got %+v", test.expectedValue, n.IsRoot())
+		}
+	}
+}
+
+func TestSetIsRoot(t *testing.T) {
+	tests := []struct {
+		isRoot        bool
+		expectedValue byte
+	}{
+		{false, 0x0},
+		{true, 0x1},
+	}
+
+	for _, test := range tests {
+		page := Page(make([]byte, PageSize))
+		n := NewNode(&page)
+		n.SetIsRoot(test.isRoot)
+
+		if page[1:2][0] != test.expectedValue {
+			t.Errorf("node is wrong type ,expected %+v, got %+v", test.expectedValue, page[1:2][0])
+
+		}
+	}
+}
